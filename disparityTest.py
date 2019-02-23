953bb93cbf3a07c6c28573e62477aadc5111028b
Liam Day <dayliam1@gmail.com>
2019-02-23 22:06:01 +0000

    - Add support for Microsoft Kinect

--- disparityTest.py
+++ disparityTest.py
@@ -11,11 +11,11 @@ Projectile Detection and Interaction
 # Python 2/3 compatibility
 from __future__ import print_function
 
-#import imutils
+import imutils
 import numpy as np
 import cv2 as cv
-from matplotlib import pyplot as plt
 import os
+
 
 def nothing(x):
         pass
@@ -26,24 +26,21 @@ def updateSGBM():
     if(_block_size % 2 == 0 & _block_size > 0):
         _block_size -= 1
     stereo.setMinDisparity(cv.getTrackbarPos('minDisparities','disparity'))
+    # NumDisparities must be divisible by 16
     stereo.setNumDisparities(cv.getTrackbarPos('numDisparities','disparity')*16)
     stereo.setBlockSize(_block_size)
     stereo.setP1(cv.getTrackbarPos('P1','disparity'))
     stereo.setP2(cv.getTrackbarPos('P2','disparity'))
     stereo.setDisp12MaxDiff(cv.getTrackbarPos('disp12MaxDiff','disparity'))
+    stereo.setPreFilterCap(cv.getTrackbarPos('preFilterCap', 'disparity'))
     stereo.setUniquenessRatio(cv.getTrackbarPos('uniquenessRatio','disparity'))
     stereo.setSpeckleWindowSize(cv.getTrackbarPos('speckleWindowSize','disparity'))
-    stereo.setSpeckleRange(cv.getTrackbarPos('speckleRange','disparity'))
+    stereo.setSpeckleRange(cv.getTrackbarPos('speckleRange','disparity')) 
+    
+    
 
 if __name__ == '__main__':
-    print('loading images...')
     
-#    cams_test = 10
-#    for i in range(0, cams_test):
-#        cap = cv.VideoCapture(i)
-#        test, frame = cap.read()
-#        print("i : "+ str(i) + "/// result: " + str(test))
-#        cap.release()
     calib_folder_path = '/home/liam/fyp/calib/calib_values/'
     cam_mats_left = np.load(os.path.join(calib_folder_path, 'cam_mats_left.npy'))
     cam_mats_right = np.load(os.path.join(calib_folder_path, 'cam_mats_right.npy'))
@@ -76,68 +73,67 @@ if __name__ == '__main__':
     num_disp = 160-min_disp
     
 #    stereo = cv.StereoBM_create(numDisparities = 32,
-#                                blockSize = 9)
-    
-#    cv.namedWindow('disparity')
-#    cv.createTrackbar('minDisparities', 'disparity', 0, 480, nothing)
-#    cv.createTrackbar('numDisparities', 'disparity', 1, 20, nothing)
-#    cv.createTrackbar('blockSize', 'disparity', 1, 31, nothing)
-#    cv.createTrackbar('P1', 'disparity', 0, 480, nothing)
-#    cv.createTrackbar('P2', 'disparity', 0, 480, nothing)
-#    cv.createTrackbar('disp12MaxDiff', 'disparity', 1, 100, nothing)
-#    cv.createTrackbar('uniquenessRatio', 'disparity', 0, 20, nothing)
-#    cv.createTrackbar('speckleWindowSize', 'disparity', 50, 200, nothing)
-#    cv.createTrackbar('speckleRange', 'disparity', 0, 16, nothing)
+#                                blockSize = 5)
+#    
+    cv.namedWindow('disparity')
+    cv.createTrackbar('minDisparities', 'disparity', -100, 480, nothing)
+    cv.createTrackbar('numDisparities', 'disparity', 1, 40, nothing)
+    cv.createTrackbar('blockSize', 'disparity', 5, 31, nothing)
+    cv.createTrackbar('P1', 'disparity', 0, 480, nothing)
+    cv.createTrackbar('P2', 'disparity', 0, 480, nothing)
+    cv.createTrackbar('disp12MaxDiff', 'disparity', 1, 100, nothing)
+    cv.createTrackbar('preFilterCap', 'disparity', 0, 100, nothing)
+    cv.createTrackbar('uniquenessRatio', 'disparity', 0, 20, nothing)
+    cv.createTrackbar('speckleWindowSize', 'disparity', 50, 200, nothing)
+    cv.createTrackbar('speckleRange', 'disparity', 5, 16, nothing)
     
     stereo = cv.StereoSGBM_create(minDisparity = min_disp,
                 numDisparities = num_disp,
                 blockSize = 16,
                 P1 = 8*3*window_size**2,
                 P2 = 32*3*window_size**2,
+                preFilterCap = 0,
                 disp12MaxDiff = 1,
                 uniquenessRatio = 100,
                 speckleWindowSize = 10,
                 speckleRange = 16)
     
+    ret1, imgL = left_capture.read()
+    ret2, imgR = right_capture.read()
+    
     while (left_capture.isOpened() & right_capture.isOpened()):
         
-        ret1, imgL = left_capture.read()
-        ret2, imgR = right_capture.read()
+        
             
         if(ret1 & ret2):
-            
+          
+            updateSGBM()
 
+    
+            undistorted_left = cv.remap(src=imgL,
+                                        map1=undistortion_map_left,
+                                        map2=rectification_map_left,
+                                        interpolation=cv.INTER_LINEAR)
             
-            imgL = np.float32(imgL)
-            imgR = np.float32(imgR)
+            undistorted_right = cv.remap(src=imgR,
+                                        map1=undistortion_map_right,
+                                        map2=rectification_map_right,
+                                        interpolation=cv.INTER_LINEAR)
             
-            undistorted_left = np.reshape(imgL, (-1,1,2))
-            undistorted_right = np.reshape(imgR, (-1,1,2))
+            undistorted_left = cv.cvtColor(undistorted_left, cv.COLOR_BGR2GRAY)
+            undistorted_right = cv.cvtColor(undistorted_right, cv.COLOR_BGR2GRAY)
             
-            undistorted_left = cv.undistortPoints(src=undistorted_left,
-                               cameraMatrix=cam_mats_left, 
-                               distCoeffs=dist_coefs_left,
-                               R=rect_trans_left,
-                               P=proj_mats_left)
-            undistorted_right = cv.undistortPoints(src=undistorted_right, 
-                               cameraMatrix=cam_mats_right, 
-                               distCoeffs=dist_coefs_right,
-                               R=rect_trans_right,
-                               P=proj_mats_right)
-            
-##            
-#            updateSGBM()
-            
-            undistorted_left = np.reshape(undistorted_left, imgL.shape)
-            undistorted_right = np.reshape(undistorted_right, imgR.shape)
+            undistorted_left = imutils.rotate(undistorted_left, 180)
+            undistorted_right = imutils.rotate(undistorted_right, 180)
 
-            cv.imshow('left', undistorted_left)
-            cv.imshow('right', undistorted_right)
 #            concat_imgLR = cv.hconcat([undistorted_left, undistorted_right])
+#        
 #            cv.imshow("left&right", concat_imgLR)
-#                  
-#            disparity = stereo.compute(imgL, imgR)
-#            cv.imshow('disparity', disparity)
+            cv.imshow('left', undistorted_left)
+            cv.imshow('right', undistorted_right)      
+            disparity = stereo.compute(undistorted_left, undistorted_right)
+            
+            cv.imshow('disparity', disparity)
             
             if cv.waitKey(1)  & 0xFF == ord('q'):
                 break
@@ -147,3 +143,58 @@ if __name__ == '__main__':
     left_capture.release()
     right_capture.release()
     cv.destroyAllWindows()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
